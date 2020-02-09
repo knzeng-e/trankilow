11709683ca3ddba051f6d6f723017ef51feec017
glennvincy <glennvincy@yahoo.fr>
2020-02-09 21:43:35 +0100

    Connecting the blockchain backend to the front to identify users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import M from 'materialize-css';
+import getWeb3 from "./getWeb3";
 import Home from './components/Home';
+import Login from './components/Login';
 import React, { Component } from 'react';
 import SignIn from './components/Signin';
 import SignUp from './components/Signup';
@@ -9,11 +11,22 @@ import Voyages from './components/Voyages';
 import SideMenu from './components/SideMenu';
 import AddTravel from './components/AddTravel';
 import UserDetails from './components/UserDetails';
+import SimpleStorageContract from "./contracts/SimpleStorage.json"
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 
  class App extends Component {
 
-  componentDidMount = () => {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      web3: null,
+      accounts: null,
+      contract: null
+    }
+  }
+
+  componentDidMount = async () => {
     const options = {
       responsiveThreshold: 0,
     }
@@ -21,6 +34,39 @@ import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-do
       var elems = document.querySelectorAll('.parallax');
       var instances = M.Parallax.init(elems, options);
     });
+
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+//       if (window.confirm('Ce site utilise la blockchain ethereum. Pour en tirez pleinement profit, veuillez connecter un portefeuille crypto. A moins que vous souhaitiez télécharger Metamask ?')) 
+// {
+// window.location.href='https://metamask.io/';
+// };
+      alert(
+        // `Failed to load web3, accounts, or contract. Check console for details.`,
+        'Ce site utilise la blockchain ethereum. Pour en tirez pleinement profit, veuillez connecter un portefeuille crypto ou installer un client comme Metamask'
+      );
+      console.error(error);
+    }
+
+
   }
    render(){
     return (
@@ -40,26 +86,7 @@ import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-do
                          <SideMenu/>
                     </li>
                   </ul>
-                  <ul className="right hide-on-med-and-down">
-                      <li>
-                          <NavLink className = "" to="/signup">Créez votre compte</NavLink>
-                      </li>
-                      <li>
-                          <NavLink className = "" to="/signin">Connexion</NavLink>
-                      </li>
-                      <li>
-                        <a href="/newTravel">Proposez un trajet</a>
-                          {/* <NavLink className = "" to="/newTravel">Proposez un trajet</NavLink> */}
-                      </li>
-                  </ul>
-                  
-                  {/* <form>
-                      <div className="input-field right">
-                          <input id="search" type="search" required/>
-                          <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
-                          <i className="material-icons">close</i>
-                      </div>
-                  </form> */}
+                  <Login accounts = {this.state.accounts}/>
               </div>
           </nav>
         {/* <SideMenu /> */}
---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = account =>  {
+    console.log("DHEGJHEB ==> ", account)
+    const etherscanSearch = "https://etherscan.io/address/" + account
+    console.log("EteherScan ==> ", etherscanSearch)
+    const userInfos = <i className="material-icons">account_circle</i>
+        return (
+            <div>
+                <ul className="right hide-on-med-and-down">
+                    <li>{userInfos}</li>
+                    <li>
+                        <div>
+                            <a href={etherscanSearch}>{account}</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/newTravel">Proposez un trajet</a>
+                    </li>
+                </ul>
+            </div>
+        )
+}
+
+const SignedOutLinks = account => {
+    return (
+        <div>
+            <ul className="right hide-on-med-and-down">
+                <li>
+                    <NavLink className = "" to="/signin">Connexion</NavLink>
+                </li>
+                <li>
+                    <NavLink className = "" to="/signup">Créez votre compte</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+const Login = ({accounts}) => {
+    //let accounts = props.accounts;
+
+    console.log("NEXT ACCOUNT STATE ==> ", accounts)
+
+    return ( accounts !== null ? 
+        (SignedInLinks(accounts)): 
+         (SignedOutLinks(accounts))
+        )
+}
+
+export default Login;
